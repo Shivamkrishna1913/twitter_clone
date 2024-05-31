f42944d409d322263c7a0bd891441d6bf2b2d72f
Shivam maurya <krishnashivam735423@gmail.com>
2024-05-31 23:37:00 +0530

    user routes and controllers done!!

--- backend/models/user.models.js
+++ backend/models/user.models.js
@@ -39,6 +39,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "",
     },
+    coverImg: {
+      type: String,
+      default: "",
+    },
     bio: {
       type: String,
       default: "",
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import { v2 as cloudinary } from "cloudinary";
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectMONGODB from "./db/connectMONGODB.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const PORT = process.env.PORT;
 
@@ -16,6 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectMONGODB();
